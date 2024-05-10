b118bcf6c7d0220b8f813da15914545234d25f61
Ines Riahi <ines.riahi@tekrnd.com>
2024-05-10 16:30:36 +0100

    create login/register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'student',
+    loadChildren: () => import('./modules/student/student.module').then(m=>m.StudentModule)
+  },
+  {
+    path:'teacher',
+    loadChildren:()=> import('./modules/teacher/teacher.module').then(m=>m.TeacherModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
